7ba50569034bc3c8f124c223e3c9d01b592294f3
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-03-14 09:14:57 +0100

    Initial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter as FontSans } from "next/font/google";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@/styles/globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <style jsx global>{`
+        html {
+          font-family: ${fontSans.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
