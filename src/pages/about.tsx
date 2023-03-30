4bcf445b4f49d1ba19995c211207aed33bf98181
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-03-30 21:55:36 +0100

    projects and about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export default function About() {
+  return (
+    <section className="mt-16">
+      <header>
+        <h1 className="text-4xl font-bold uppercase pl-8">About</h1>
+        <p className="pl-8 text-2xl font-medium capitalize mt-4">Coming soon 👀</p>
+      </header>
+    </section>
+  )
+}
